336ebcefdaeace6f854081550435a4881bc1d7ab
SamanFekri <samanf74@gmail.com>
2016-06-02 16:39:02 +0430

    chess mans added on table

--- js/chess.js
+++ js/chess.js
@@ -12,11 +12,21 @@ function loadChessGame(dir) {
     if(DOMOfChessXml == null){
         synchronousAjax(chessdir,analyzeChess);
     }
+    whiteOutMan=[];
+    blackOutMan=[];
+    whiteInMan=[] ;
+    blackInMan=[];
+    chessCells ="";
+    whiteScore="";
+    blackScore="";
+    whiteField="";
+    blackField="";
+    unicodes=[];
     makeChess();
 }
 function makeChess() {
     $('#main-container').html("");
-    chessCells = Array(8);
+    chessCells = [];
 
     unicodes = unicodesMaker();
     chessmanToken();
@@ -24,9 +34,57 @@ function makeChess() {
     var chessDiv = jQuery("<div/>").attr("id","chess");
     $('#main-container').append(chessDiv);
     chessDiv.append(makeChessInfo());
-    chessDiv.append(makeChessManPanel("white"));
+    chessDiv.append(makeChessManPanel("white",whiteOutMan));
     chessDiv.append(makeChessPanel());
-    chessDiv.append(makeChessManPanel("black"));
+    chessDiv.append(makeChessManPanel("black",blackOutMan));
+}
+function haveChessman(i,j,td){
+    for(var p = 0; p < whiteInMan.length; p++){
+        if(whiteInMan[p][1] == i && whiteInMan[p][2] == j){
+            td.css("color","white");
+            return whiteInMan[p][0];
+        }
+    }
+    for(var p = 0; p < blackInMan.length; p++){
+        if(blackInMan[p][1] == i && blackInMan[p][2] == j){
+            td.css("color","black");
+            return blackInMan[p][0];
+        }
+    }
+    return "";
+}
+function makeChessPanel(){
+    var board = DOMOfChessXml.getElementsByTagName('board')[0];
+    var whiteCellColor = board.getAttribute('white-cells');
+    var blackCellColor = board.getAttribute('black-cells');
+    var chessTable = jQuery("<table/>");
+    for (var i = 0; i < 8; i++){
+        var tr = jQuery("<tr/>");
+        var mytmp = Array(8);
+        for (var j = 0; j < 8; j++){
+            var td = jQuery("<td/>");
+            mytmp[j] = td;
+            if( i % 2 == j % 2){
+                td.css("background-color",whiteCellColor);
+            } else {
+                td.css("background-color",blackCellColor);
+            }
+            td.append(haveChessman(i,j,td));
+            tr.append(td);
+        }
+        chessCells.push(mytmp);
+        chessTable.append(tr);
+    }
+    console.log('chess array');
+    console.log(chessCells);
+    return chessTable;
+}
+function makeChessManPanel(color,outMans){
+    var chessmanPanel = jQuery("<div/>").attr("id",color+"-chessman-panel").css("color",color);
+    for  (var i = 0; i<outMans.length;i++){
+        chessmanPanel.append(outMans[i]);
+    }
+    return chessmanPanel;
 }
 
 function chessmanToken(){
@@ -76,30 +134,7 @@ function unicodesMaker(){
     return ret;
 }
 
-function makeChessPanel(){
-    var board = DOMOfChessXml.getElementsByTagName('board')[0];
-    var whiteCellColor = board.getAttribute('white-cells');
-    var blackCellColor = board.getAttribute('black-cells');
-    var chessTable = jQuery("<table/>");
-    for (var i = 0; i < 8; i++){
-        var tr = jQuery("<tr/>");
-        for (var j = 0; j < 8; j++){
-            var td = jQuery("<td/>");
-            if( i % 2 == j % 2){
-                td.css("background-color",whiteCellColor);
-            } else {
-                td.css("background-color",blackCellColor);
-            }
-            tr.append(td);
-        }
-        chessTable.append(tr)
-    }
-    return chessTable;
-}
-function makeChessManPanel(color){
-    var chessmanPanel = jQuery("<div/>").attr("id",color+"-chessman-panel");
-    return chessmanPanel;
-}
+
 function makeChessInfo() {
     var infoDiv = jQuery("<div/>").attr("id","chess-info");
 
