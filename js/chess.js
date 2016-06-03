c45c28c4f9a038dfd458631491dc2e3a54850aae
SamanFekri <samanf74@gmail.com>
2016-06-03 16:41:07 +0430

    castle move added

--- js/chess.js
+++ js/chess.js
@@ -29,7 +29,9 @@ function loadChessGame(dir) {
     selectedPiece = null;
     selectedCanGo = null;
 
-    castle = Array(2).fill(0);
+    castle = [];
+    castle.push([0,0]);
+    castle.push([0,0]);
     makeChess();
 }
 function makeChess() {
@@ -45,6 +47,9 @@ function makeChess() {
     chessDiv.append(makeChessManPanel("white",whiteOutMan));
     chessDiv.append(makeChessPanel());
     chessDiv.append(makeChessManPanel("black",blackOutMan));
+
+    //castle check
+    castleFirstCheck();
 }
 function selectChessCell(event) {
     var cell = chessCells[event.data.x][event.data.y];
@@ -90,79 +95,178 @@ function whereCanGo(cell,x,y) {
                 return(pawnMoves(x,y,blackField));
             }
             break;
+        case "knight":
+            return(knightMoves(x,y));
+            break;
+        case "bishop":
+            return(bishopMoves(x,y));
+            break;
+        case "rook":
+            var ret_val = rookMoves(x,y);
+            return ret_val;
+            break;
+        case "queen":
+            var ret_val = rookMoves(x,y);
+            ret_val = ret_val.concat(bishopMoves(x,y));
+            return ret_val;
+            break;
+        case "king":
+            return(kingMoves(x,y));
+            break;
     }
 }
-function attack(cell) {
-    var type = whoIs(cell.attr("x"),cell.attr("y"));
-    if(checkCanGo(cell.attr("x"),cell.attr("y"))){
-        if(type == "king"){
-            return false;
-        }else{
-            var score = Number($("#"+selectedPiece.attr('owner')+"-score").html());
-            score += pieceScore[type];
-            $("#"+selectedPiece.attr('owner')+"-score").html(score);
-            $("#"+cell.attr('owner')+"-chessman-panel").append(cell.html());
-            if(cell.attr('owner') == "white"){
-                whiteOutMan.push(cell.html());
-            }else{
-                blackOutMan.push(cell.html());
+function kingMoves(x,y) {
+    var retVal = [];
+    var mytmp = [];
+    mytmp.push([x+1,y+1]);
+    mytmp.push([x+1,y-1]);
+    mytmp.push([x-1,y+1]);
+    mytmp.push([x-1,y-1]);
+    mytmp.push([x+1,y]);
+    mytmp.push([x-1,y]);
+    mytmp.push([x,y+1]);
+    mytmp.push([x,y-1]);
+    for(var i =0; i < mytmp.length; i++){
+        if(mytmp[i][0] < 8 && mytmp[i][0] > -1 && mytmp[i][1] < 8 && mytmp[i][1] > -1 ){
+            if(chessCells[mytmp[i][0]][mytmp[i][1]].attr('owner') != chessCells[x][y].attr('owner')){
+                retVal.push([mytmp[i][0],mytmp[i][1]]);
             }
-            return true;
         }
-    } else {
-        return false;
     }
+    var mycastle = castleMoves(x,y);
+    if(mycastle != null){
+        retVal = retVal.concat(mycastle);
+    }
+    console.log(retVal);
+    return retVal;
 }
-function move(cell,x,y) {
-    if(checkCanGo(x,y)){
-        cell.html(selectedPiece.html());
-        cell.css("color",selectedPiece.attr('owner'));
-        cell.attr("owner",selectedPiece.attr('owner'));
+function rookMoves(x,y) {
+    var mytmp = [];
 
-        selectedPiece.html("");
-        selectedPiece.attr('owner',"none");
-        selectedPiece.css('color',"");
-        deselectAllCell();
-        if(x % 2 == y % 2){
-            cell.css("background-color","lime");
-        }else{
-            cell.css("background-color","green");
+    var i = 1;
+    while (x + i < 8){
+        if (chessCells[x + i][y].attr('owner') != "none") {
+            if (chessCells[x + i][y].attr('owner') != chessCells[x][y].attr('owner')) {
+                mytmp.push([x + i, y]);
+            }
+            break;
+        } else {
+            mytmp.push([x + i, y]);
         }
-        if(selectedPiece.attr('x') % 2 == selectedPiece.attr('y') % 2){
-            selectedPiece.css("background-color","lime");
-        }else{
-            selectedPiece.css("background-color","green");
-        }
-        updateInMan();
-        setTimeout(function () {
-            deselectAllCell();
-        },500);
-    }else{
-        deselectAllCell();
+        i++;
     }
-    selectedPiece = null;
-    selectedCanGo = null;
-}
-function checkCanGo(x,y) {
-    for(var i = 0; i < selectedCanGo.length; i++){
-        if(selectedCanGo[i][0] == x && selectedCanGo[i][1] == y){
-            return true;
+
+    i = 1;
+    while (x - i > -1){
+        if (chessCells[x - i][y].attr('owner') != "none") {
+            if (chessCells[x - i][y].attr('owner') != chessCells[x][y].attr('owner')) {
+                mytmp.push([x - i, y]);
+            }
+            break;
+        } else {
+            mytmp.push([x - i, y]);
         }
+        i++;
     }
-    return false;
+
+    i = 1;
+    while (y + i < 8){
+        if (chessCells[x][y + i].attr('owner') != "none") {
+            if (chessCells[x][y + i].attr('owner') != chessCells[x][y].attr('owner')) {
+                mytmp.push([x, y + i]);
+            }
+            break;
+        } else {
+            mytmp.push([x, y + i]);
+        }
+        i++;
+    }
+
+    i = 1;
+    while (y - i > -1){
+        if (chessCells[x][y - i].attr('owner') != "none") {
+            if (chessCells[x][y - i].attr('owner') != chessCells[x][y].attr('owner')) {
+                mytmp.push([x, y - i]);
+            }
+            break;
+        } else {
+            mytmp.push([x, y - i]);
+        }
+        i++;
+    }
+    return mytmp;
 }
-function updateInMan() {
-    whiteInMan = [];
-    blackInMan = [];
-    for (var i = 0; i < chessCells.length; i++){
-        for (var j = 0; j < chessCells[i].length; j++){
-            if(chessCells[i][j].attr('owner') == "white"){
-                whiteInMan.push([chessCells[i][j].html(),i,j]);
-            }else{
-                blackInMan.push([chessCells[i][j].html(),i,j]);
+function bishopMoves(x,y) {
+    var mytmp = [];
+    for(var i = 1; i < 8; i++) {
+        if (x + i < 8 && y + i < 8) {
+            if (chessCells[x + i][y + i].attr('owner') != "none") {
+                if (chessCells[x + i][y + i].attr('owner') != chessCells[x][y].attr('owner')) {
+                    mytmp.push([x + i, y + i]);
+                }
+                break;
+            } else {
+                mytmp.push([x + i, y + i]);
             }
         }
     }
+    for(var i = 1; i < 8; i++) {
+        if (x - i > -1 && y + i < 8) {
+            if (chessCells[x - i][y + i].attr('owner') != "none") {
+                if (chessCells[x - i][y + i].attr('owner') != chessCells[x][y].attr('owner')) {
+                    mytmp.push([x - i, y + i]);
+                }
+                break;
+            } else {
+                mytmp.push([x - i, y + i]);
+            }
+        }
+    }
+    for(var i = 1; i < 8; i++) {
+        if(x + i < 8&& y - i >-1) {
+            if (chessCells[x + i][y - i].attr('owner') != "none") {
+                if (chessCells[x + i][y - i].attr('owner') != chessCells[x][y].attr('owner')) {
+                    mytmp.push([x + i, y - i]);
+                }
+                break;
+            } else {
+                mytmp.push([x + i, y - i]);
+            }
+        }
+    }
+    for(var i = 1; i < 8; i++) {
+        if(x-i > -1&& y-i > -1) {
+            if (chessCells[x - i][y - i].attr('owner') != "none") {
+                if (chessCells[x - i][y - i].attr('owner') != chessCells[x][y].attr('owner')) {
+                    mytmp.push([x - i, y - i]);
+                }
+                break;
+            } else {
+                mytmp.push([x - i, y - i]);
+            }
+        }
+    }
+    return mytmp;
+}
+function knightMoves(x,y) {
+    var retVal = [];
+    var mytmp = [];
+    mytmp.push([x+2,y+1]);
+    mytmp.push([x+2,y-1]);
+    mytmp.push([x-2,y+1]);
+    mytmp.push([x-2,y-1]);
+    mytmp.push([x+1,y+2]);
+    mytmp.push([x-1,y+2]);
+    mytmp.push([x+1,y-2]);
+    mytmp.push([x-1,y-2]);
+    for(var i =0; i < mytmp.length; i++){
+        if(mytmp[i][0] < 8 && mytmp[i][0] > -1 && mytmp[i][1] < 8 && mytmp[i][1] > -1 ){
+            if(chessCells[mytmp[i][0]][mytmp[i][1]].attr('owner') != chessCells[x][y].attr('owner')){
+                retVal.push([mytmp[i][0],mytmp[i][1]]);
+            }
+        }
+    }
+    return retVal;
 }
 function pawnMoves(x,y,moveDir) {
     var retVal = [];
@@ -223,6 +327,213 @@ function pawnMoves(x,y,moveDir) {
     }
     return retVal;
 }
+function castleFirstCheck() {
+    //console.log("castle");
+    var filedColor;
+    if(whiteField == "top"){
+        filedColor = {top:"white",bot:"black"}
+    }else{
+        filedColor = {bot:"white",top:"black"}
+    }
+    var index = {white:0,black:1};
+
+    if(chessCells[0][3].attr('owner') == filedColor['bot']){
+
+        if (whoIs(0, 3) != "king") {
+            castle[index[filedColor['bot']]][0] = 1;
+            castle[index[filedColor['bot']]][1] = 1;
+        }else{
+            if (chessCells[0][0].attr('owner') == filedColor['bot']) {
+                if (whoIs(0, 0) != "rook") {
+                    castle[index[filedColor['bot']]][0] = 1;
+                }
+            } else {
+                castle[index[filedColor['bot']]][0] = 1;
+            }
+            if (chessCells[0][7].attr('owner') == filedColor['bot']) {
+                if (whoIs(0, 7) != "rook") {
+                    castle[index[filedColor['bot']]][1] = 1;
+                }
+            } else {
+                castle[index[filedColor['bot']]][1] = 1;
+            }
+        }
+    }else {
+        castle[index[filedColor['bot']]][0] = 1;
+        castle[index[filedColor['bot']]][1] = 1;
+    }
+    if(chessCells[7][3].attr('owner') == filedColor['top']) {
+        if (whoIs(7, 3) != "king") {
+            castle[index[filedColor['top']]][0] = 1;
+            castle[index[filedColor['top']]][1] = 1;
+        } else {
+            if (chessCells[7][0].attr('owner') == filedColor['top']) {
+                if (whoIs(7, 0) != "rook") {
+                    castle[index[filedColor['top']]][0] = 1;
+                }
+            } else {
+                castle[index[filedColor['top']]][0] = 1;
+            }
+            if (chessCells[7][7].attr('owner') == filedColor['top']) {
+                if (whoIs(7, 7) != "rook") {
+                    castle[index[filedColor['top']]][1] = 1;
+                }
+            } else {
+                castle[index[filedColor['top']]][1] = 1;
+            }
+        }
+    }else{
+        castle[index[filedColor['top']]][0] = 1;
+        castle[index[filedColor['top']]][1] = 1;
+
+    }
+
+    /*console.log(castle);
+    console.log(index[filedColor['bot']] + " > " + filedColor['bot']);
+    console.log(index[filedColor['top']] + " > " + filedColor['top']);*/
+}
+function castleMoves(x,y) {
+    var ret_val = [];
+    var index = {white:0,black:1};
+    if(castle[index[chessCells[x][y].attr('owner')]][0] == 0){
+        if(chessCells[x][1].attr('owner') == 'none' &&
+            chessCells[x][2].attr('owner') == 'none'){
+            ret_val.push([x,1]);
+        }
+    }
+    if(castle[index[chessCells[x][y].attr('owner')]][1] == 0){
+        if(chessCells[x][4].attr('owner') == 'none' &&
+            chessCells[x][5].attr('owner') == 'none' &&
+            chessCells[x][6].attr('owner') == 'none'){
+            ret_val.push([x,5]);
+        }
+    }
+    //console.log(ret_val);
+    if(x == 0 || x == 7){
+        return ret_val;
+    } else{
+        return null;
+    }
+}
+
+function promotion(x,y) {
+    var type = whoIs(x,y);
+    var direction;
+    if(chessCells[x][y].attr("owner") == "white"){
+        direction = whiteField;
+    }else{
+        direction = blackField;
+    }
+    console.log(x + " > " + y)
+    console.log(direction);
+    if(type == "pawn") {
+        if (x == 0 && direction == "top") {
+            chessCells[x][y].html(unicodes["queen"]);
+        } else if (x == 7 && direction == "bottom") {
+            chessCells[x][y].html(unicodes["queen"]);
+        }
+        console.log("here");
+    }
+
+}
+
+function attack(cell) {
+    var type = whoIs(cell.attr("x"),cell.attr("y"));
+    if(checkCanGo(cell.attr("x"),cell.attr("y"))){
+        if(type == "king"){
+            return false;
+        }else{
+            var score = Number($("#"+selectedPiece.attr('owner')+"-score").html());
+            score += pieceScore[type];
+            $("#"+selectedPiece.attr('owner')+"-score").html(score);
+            $("#"+cell.attr('owner')+"-chessman-panel").append(cell.html());
+            if(cell.attr('owner') == "white"){
+                whiteOutMan.push(cell.html());
+            }else{
+                blackOutMan.push(cell.html());
+            }
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+function move(cell,x,y) {
+    if(checkCanGo(x,y)){
+        cell.html(selectedPiece.html());
+        cell.css("color",selectedPiece.attr('owner'));
+        cell.attr("owner",selectedPiece.attr('owner'));
+        // for castle move
+        //console.log("here3");
+        //console.log(whoIs(selectedPiece.attr("x"),selectedPiece.attr("y")));
+        if(whoIs(selectedPiece.attr("x"),selectedPiece.attr("y")) == "king"){
+            //console.log("here");
+            if(selectedPiece.attr("y") - y > 1){
+                //console.log("here2");
+                xchangeCell(chessCells[selectedPiece.attr("x")][0],chessCells[selectedPiece.attr("x")][2]);
+            }else if( selectedPiece.attr("y") - y < -1 ) {
+                //console.log("here4");
+                xchangeCell(chessCells[selectedPiece.attr("x")][7],chessCells[selectedPiece.attr("x")][4]);
+            }
+        }
+        selectedPiece.html("");
+        selectedPiece.attr('owner',"none");
+        selectedPiece.css('color',"");
+        deselectAllCell();
+        if(x % 2 == y % 2){
+            cell.css("background-color","lime");
+        }else{
+            cell.css("background-color","green");
+        }
+        if(selectedPiece.attr('x') % 2 == selectedPiece.attr('y') % 2){
+            selectedPiece.css("background-color","lime");
+        }else{
+            selectedPiece.css("background-color","green");
+        }
+        promotion(x,y);
+        updateInMan();
+        castleFirstCheck();
+        //console.log(castle);
+        setTimeout(function () {
+            deselectAllCell();
+        },500);
+
+    }else{
+        deselectAllCell();
+    }
+    selectedPiece = null;
+    selectedCanGo = null;
+}
+function xchangeCell(from,to) {
+    to.html(from.html());
+    to.css("color",from.attr('owner'));
+    to.attr("owner",from.attr('owner'));
+    from.html("");
+    from.attr('owner',"none");
+    from.css('color',"");
+}
+function checkCanGo(x,y) {
+    for(var i = 0; i < selectedCanGo.length; i++){
+        if(selectedCanGo[i][0] == x && selectedCanGo[i][1] == y){
+            return true;
+        }
+    }
+    return false;
+}
+function updateInMan() {
+    whiteInMan = [];
+    blackInMan = [];
+    for (var i = 0; i < chessCells.length; i++){
+        for (var j = 0; j < chessCells[i].length; j++){
+            if(chessCells[i][j].attr('owner') == "white"){
+                whiteInMan.push([chessCells[i][j].html(),i,j]);
+            }else{
+                blackInMan.push([chessCells[i][j].html(),i,j]);
+            }
+        }
+    }
+}
+
 function colorWhereCanGo(arrayCanGo) {
     for (var i = 0; i < arrayCanGo.length; i++){
         if(chessCells[arrayCanGo[i][0]][arrayCanGo[i][1]].attr('owner') == "none"){
